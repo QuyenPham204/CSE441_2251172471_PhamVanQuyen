4a1e0a31ee1760e2c2164c02fb2c5a4209857160
[200~r.name John <thptdaian.phamvanquyenk1922@gmail.com>
2025-05-14 15:44:29 +0700

    BTTH

--- Exercise2/app/src/main/java/com/example/exercise2/MainActivity.java
+++ Exercise2/app/src/main/java/com/example/exercise2/MainActivity.java
@@ -8,17 +8,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button btncall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
+        btncall = findViewById(R.id.btncall);
+        btncall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+                Intent intent1 = new Intent(MainActivity.this, subactivity.class);
+                startActivity(intent1);
+            }
         });
     }
 }
