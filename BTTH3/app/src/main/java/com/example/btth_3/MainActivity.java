cfb08aef57d5aace7b9e527b839bc7b3bc8b6952
[200~r.name John <thptdaian.phamvanquyenk1922@gmail.com>
2025-06-06 01:08:22 +0700

    update bài tập

--- BTTH3/app/src/main/java/com/example/btth_3/MainActivity.java
+++ BTTH3/app/src/main/java/com/example/btth_3/MainActivity.java
@@ -1,54 +1,52 @@
 package com.example.btth_3;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends Activity {
+    private Button buttonStartTask;
+    private TextView textViewProgress;
+    private static final String TAG = "BackgroundTaskDemo";
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-public class MainActivity extends AppCompatActivity {
+    buttonStartTask = findViewById(R.id.buttonStartTask);
+    textViewProgress = findViewById(R.id.textViewProgress);
 
-    DatabaseReference ref;
-    List<Player> players;
-    PlayerAdapter adapter;
-    RecyclerView recyclerView;
-    Button btnAdd;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ref = FirebaseDatabase.getInstance().getReference("players");
-        players = new ArrayList<>();
-        adapter = new PlayerAdapter(this, players);
-        recyclerView = findViewById(R.id.recyclerView);
-        btnAdd = findViewById(R.id.btnAdd);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(adapter);
-
-        ref.addValueEventListener(new ValueEventListener() {
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                players.clear();
-                for (DataSnapshot snap : snapshot.getChildren()) {
-                    Player p = snap.getValue(Player.class);
-                    players.add(p);
+    buttonStartTask.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+//            String result = dowloaddate();
+//            textViewProgress.setText(result);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String result = dowloaddate();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textViewProgress.setText(result);
+                        }
+                    });
                 }
-                adapter.notifyDataSetChanged();
-            }
+            });
+        }
+    });
+}
 
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(MainActivity.this, "Lỗi đọc dữ liệu", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnAdd.setOnClickListener(v -> showAddDialog());
+    private String dowloaddate() {
+        try{
+            Thread.sleep(10000);
+            return "OK";
+        }
+        catch (InterruptedException e){
+            throw  new RuntimeException(e);
+        }
     }
 
-    private void showAddDialog() {
-        // Tạo AlertDialog nhập tên, quê quán, tự sinh member_code, push lên Firebase
-    }
 }
